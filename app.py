5ba31d83ef74979dbda39be942b2640d2edfd682
saumilkhera1 <saumilkhera1@gmail.com>
2026-07-27 13:14:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def prompt_generator(agent):
   with open(file_name, 'w') as f:
     f.write(response.content[-1]['text'])
   return "Prompt file generated Successfully, agent can read it"
-
+prompt_generator(model)
 # tool 2
 
 def resume_maker_prompt():
@@ -80,7 +80,7 @@ def resume_maker_prompt():
   with open('prompt.py', 'r') as f:
     prompt = f.read()
   return prompt
-
+resume_maker_prompt()
 
 # ===================Generator====================
 prompt = """you are a helpful ai assistant
@@ -99,6 +99,7 @@ if st.button("Generate resume"):
     response = agent.invoke({'messages': [{'role':'user','content':query}]})
     code = response['messages'][-1].content[-1]['text']
 
-    st.mardown(code)
+    #st.mardown(code)
+    st.html(code, width="stretch",unsafe_allow_javascript=True)
 
 
