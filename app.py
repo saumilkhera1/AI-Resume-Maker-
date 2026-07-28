92123a2f7f9f19490c35479def2213acc258fe80
saumilkhera1 <saumilkhera1@gmail.com>
2026-07-28 13:24:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,19 @@ user_details=f"""user details:given beow :resume info {USER_INFO} DEFAULT IF NOT
 query = final_prompt+user_details
 
 import base64
+OPTIONS = ["DELHI","NOIDA","GURUGRAM","KANPUR","LUCKNOW","BANGLORE","PUNE"]
+LOCATION = st.sidebar.multiselect('SELECT LOCATION :- ',
+                                 options = OPTIONS)
+JOB_PROFILE = ["PYTHON DEVLOPER","GEN AI EXPERT","FULLSTACK DEVELPER"]
+PROFILE = st.sidebar.multiselect('SELECT PROFILES :- ',
+                                 options = PROFILES)
 
+job_prompt = f"""Based on {PROFILE} jobs in {LOCATION}, I
+want latest job news in using tavily,
+try top 10 search or whatever available
+and give result like naukri theme design with
+job name, job desc, salary,
+apply link"""
 if st.button('generate resume'):
   with st.spinner("runnign agent"):
 
@@ -134,3 +146,7 @@ if st.button('generate resume'):
       
 
     st.html(code , width="stretch" , unsafe_allow_javascript=True)
+    st.divider()
+    response = agent.invoke({'messages': [{'role': 'user', 'content':job_prompt}]})
+    job_code = response['messages'][-1]).content[-1]['text']
+    st.html(job_code width="stretch", unsafe_allow_javascript=True)
